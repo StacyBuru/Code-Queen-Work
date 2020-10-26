b9a09b2a95006e41db03c997ed28088ed28e06f9
StacyBuru <38223688+StacyBuru@users.noreply.github.com>
2020-10-26 17:26:32 +0300

    array practise

---
+++ arrays_practise/hoArray.js
@@ -0,0 +1,38 @@
+// Task 1: Update the variable songList so that it is an array with at least 3 song titles.
+let songList = ["so serious", "addicted", "sunday"];
+console.log(songList);
+
+
+// Task 2: Update the variable classList so that it is an array with 4 Code Nation students (including yourself) in your class.
+let classList = ["Stacy", "Daisy", "Fanny"];
+console.log(classList);
+
+// Task 3: Update the variable teacherList so that it is an array with all the Code Nation adults in your class.
+let teacherList = ["Frank", "Jude", "Lydia"];
+console.log(teacherList);
+
+// Task 4: Update the variable familyAges so that it is an array with a list of ages (numbers) of your family members.
+let familyAges = [22, 19, 30];
+console.log(familyAges);
+
+//PAUSE HERE
+
+/*
+	PART 2 
+ */
+
+// Task 5: Update the variable favSong to retrieve the array element of your favorite song in the songList array (Task 1). 
+let favSong = songList[2];
+console.log(favSong);
+
+// Task 6: Update the variable bestStudent to retrieve your name from the classList array (Task 2).
+let bestStudent = classList[0];
+console.log(bestStudent);
+
+// Task 7: Update the variable tallestTeacher to retrieve the array element of the tallest teacher in the teacherList array (Task 3).
+let tallestTeacher = teacherList[1];
+console.log(tallestTeacher);
+
+// Task 8: Update the variable oldestMemeber to retrieve the array element of the oldest member of your family in the familyAges array. (Task 4)
+let oldestMember = familyAges[2];
+console.log(oldestMember);
